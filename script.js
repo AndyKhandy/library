ce1344de6780070f613201965984f6a9f8b7162c
AndyKhandy <177374520+AndyKhandy@users.noreply.github.com>
2025-11-09 14:44:18 -0600

    Finish basic styling for dialog element + begin JS logic

--- script.js
+++ script.js
@@ -1,6 +1,12 @@
 const promptAddBook = document.querySelectorAll(".new");
+
 const favDialog = document.querySelector("#favDialog");
 const cancelBtn = document.querySelector("#cancel");
+
+const titleInput = document.querySelector("#titleInput");
+const authorInput = document.querySelector("#authorInput");
+const pagesInput = document.querySelector("#pagesInput");
+const readInput = document.querySelector("#readInput");
 
 promptAddBook.forEach(element => {
     element.addEventListener("click", () => {
