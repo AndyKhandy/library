754008d7455681ecb72f47b54b2c5eaca03e30ae
AndyKhandy <177374520+AndyKhandy@users.noreply.github.com>
2025-11-11 15:51:18 -0600

    Create heart and currently reading book functionality

--- script.js
+++ script.js
@@ -81,11 +81,25 @@ function displayBook()
         let read = document.createElement("h3");
         read.textContent = `Read: ${myLibrary[i].read ? "Yes" : "No"}`;
 
+        let heart = document.createElement("button");
+        heart.classList.add("heart", "bookBtn");
+        heart.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path d="M12.1,18.55L12,18.65L11.89,18.55C7.14,14.24 4,11.39 4,8.5C4,6.5 5.5,5 7.5,5C9.04,5 10.54,6 11.07,7.36H12.93C13.46,6 14.96,5 16.5,5C18.5,5 20,6.5 20,8.5C20,11.39 16.86,14.24 12.1,18.55M16.5,3C14.76,3 13.09,3.81 12,5.08C10.91,3.81 9.24,3 7.5,3C4.42,3 2,5.41 2,8.5C2,12.27 5.4,15.36 10.55,20.03L12,21.35L13.45,20.03C18.6,15.36 22,12.27 22,8.5C22,5.41 19.58,3 16.5,3Z" /></svg>`;
+
+        heart.addEventListener("click", ()=>{
+            heart.classList.toggle("active");
+        });
+
+        let currentlyReading = document.createElement("h2");
+        currentlyReading.textContent = "CURRENTLY READING";
+        currentlyReading.classList.add("currentBook");
+        cardInfo.appendChild(currentlyReading);
+
         cardInfo.appendChild(completed);
         cardInfo.appendChild(title);
         cardInfo.appendChild(author);
         cardInfo.appendChild(pages);
         cardInfo.appendChild(read);
+        cardInfo.appendChild(heart);
 
         let buttonDiv = document.createElement("div");
         buttonDiv.classList.add("flex","flex-gap");
