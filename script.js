c941c7e88c445fe9dbc5649409e8b3a9ef6e068f
AndyKhandy <177374520+AndyKhandy@users.noreply.github.com>
2025-11-11 15:13:51 -0600

    Change out text in buttons to icons to make website more visually appealing

--- script.js
+++ script.js
@@ -25,6 +25,7 @@ function Book(title,author,pages,pagesR,read,id)
     this.pagesR = pagesR;
     this.read = read;
     this.id = id;
+    this.goal = false;
 }
 
 let starterBook = new Book("Harry Potter", "J.K Rowling", 4100, 3400, false, "1");
@@ -90,8 +91,21 @@ function displayBook()
         buttonDiv.classList.add("flex","flex-gap");
 
         let delBtn = document.createElement("button");
-        delBtn.textContent = "Delete";
-        delBtn.addEventListener("click", ()=>{
+        delBtn.classList.add("delete", "bookBtn");
+        buttonDiv.appendChild(delBtn);
+        delBtn.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path d="M9,3V4H4V6H5V19A2,2 0 0,0 7,21H17A2,2 0 0,0 19,19V6H20V4H15V3H9M7,6H17V19H7V6M9,8V17H11V8H9M13,8V17H15V8H13Z" /></svg>`;
+
+        let finishBtn = document.createElement("button");
+        finishBtn.classList.add("finish", "bookBtn");
+        buttonDiv.appendChild(finishBtn);
+        finishBtn.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path d="M19,19H5V5H15V3H5C3.89,3 3,3.89 3,5V19A2,2 0 0,0 5,21H19A2,2 0 0,0 21,19V11H19M7.91,10.08L6.5,11.5L11,16L21,6L19.59,4.58L11,13.17L7.91,10.08Z" /></svg>`;
+
+        let editBtn = document.createElement("button");
+        editBtn.classList.add("edit", "bookBtn");
+        buttonDiv.appendChild(editBtn);
+        editBtn.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path d="M18.13 12L19.39 10.74C19.83 10.3 20.39 10.06 21 10V9L15 3H5C3.89 3 3 3.89 3 5V19C3 20.1 3.89 21 5 21H11V19.13L11.13 19H5V5H12V12H18.13M14 4.5L19.5 10H14V4.5M19.13 13.83L21.17 15.87L15.04 22H13V19.96L19.13 13.83M22.85 14.19L21.87 15.17L19.83 13.13L20.81 12.15C21 11.95 21.33 11.95 21.53 12.15L22.85 13.47C23.05 13.67 23.05 14 22.85 14.19Z" /></svg>`
+
+         delBtn.addEventListener("click", ()=>{
             for(let j = 0; j < myLibrary.length; j++)
             {
                 if(card.dataset.bookId === myLibrary[j].id)
@@ -104,29 +118,22 @@ function displayBook()
             displayBook();
         });
 
-        buttonDiv.appendChild(delBtn);
-
-        if(!myLibrary[i].read)
-        {
-            let finishBtn = document.createElement("button");
-
-            finishBtn.classList.add("finish");
-            finishBtn.textContent = "Finish";
-            buttonDiv.appendChild(finishBtn);
-
-             let editBtn = document.createElement("button");
-             editBtn.classList.add("edit");
-             editBtn.style["background-image"] = 'url("img/editPage.svg")';
-
-             finishBtn.addEventListener("click", ()=>{
-                read.textContent = `Read: Yes`;
-                myLibrary[i].read = true;
-                buttonDiv.removeChild(finishBtn);
-                buttonDiv.removeChild(editBtn);
+        finishBtn.addEventListener("click", ()=>{
+            if(myLibrary[i].read == false && myLibrary[i].goal == false)
+            {
                 changeMeter();
-            });
+                myLibrary[i].read = true;
+                myLibrary[i].goal = true;
+            }
+            else {
+                myLibrary[i].read = !myLibrary[i].read;
+            }
 
-            editBtn.addEventListener("click",()=>{
+            myLibrary[i].read ? read.textContent = "Read: Yes" : read.textContent = "Read: No";
+            finishBtn.classList.toggle("active");
+        });
+
+        editBtn.addEventListener("click",()=>{
             let newPages = prompt("What page are you on now?");
             if(newPages != null && newPages != "" && newPages <= myLibrary[i].pages && newPages > 0)
             {
@@ -135,21 +142,17 @@ function displayBook()
                 {
                     const clickEvent = new Event("click");
                     finishBtn.dispatchEvent(clickEvent);
-                    changeMeter();
                 }
                 displayBook();
             }
-            })
-
-            buttonDiv.appendChild(editBtn);
-        }
-
+        })
 
         card.appendChild(cardInfo);
         card.appendChild(buttonDiv);
         libraryDisplay.appendChild(card);
     }
 }
+
 
 promptAddBook.forEach(element => {
     element.addEventListener("click", () => {
